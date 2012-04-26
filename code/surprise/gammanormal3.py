0878d87427dbf5d4aa4be5929d3b3f9e80d009e2
Wesley Brooks <brooks.wesley@gmail.com>
2012-04-26 18:06:30 -0500

    moment estimators now working in gammanormal3.py

--- code/surprise/gammanormal3.py
+++ code/surprise/gammanormal3.py
@@ -12,7 +12,7 @@ class GammaNormal(surprise.SurpriseDistribution):
         super(GammaNormal, self).__init__()
         self.window = 10
         self.n = 0
-        self.parameters = {'a':1, 'b':1, 'p':1, 'm':1}
+        self.parameters = {'a':1, 'b':1, 'm':1}
         self.sufficient = {'SS':0, 'ybar':0, 'n':0}
         self.lagged = {'SS':0, 'ybar':0, 'n':0}
         
@@ -45,13 +45,18 @@ class GammaNormal(surprise.SurpriseDistribution):
         m3 = np.mean(data**3)
         m4 = np.mean(data**4)
         
+        print m1
+        print m2
+        print m3
+        print m4
+                
+        cm4 = m4 - 4*m1*m3 + 6*m2*m1**2 - 3*m1**4
+        cm2 = (m2-m1**2)
         est = {}
-        est['m'] = m1
         
-        A = (m4 - 4*m1*m3 + 6*m1**2*m2 - 3*m1**4) / (m2-m1**2)**2
-        est['a'] = (4*A-2) / (A-3)
-        est['t'] = 1
-        est['b'] = (m2-m1**2) * (est['a']-2) / 4.
+        est['m'] = m1        
+        est['a'] = 2 + 3*cm2**2/(4*cm4)
+        est['b'] = (est['a']-1) * (m2-m1**2) / 2
         
         return est
         
@@ -59,12 +64,10 @@ class GammaNormal(surprise.SurpriseDistribution):
     def _SurpriseCalculation(self, old, new):
         a1 = old['a']
         b1 = old['b']
-        t1 = old['p']
         m1 = old['m']
         
         a2 = new['a']
         b2 = new['b']
-        t2 = new['p']
         m2 = new['m']
         
         return (a1-a2)*(digamma(a1)-log(b1)) + a1*log(b1) - a2*log(b2) + 0.5*(log(t1/t2) + 1 - t2/t1 + a1*t1/b1*(m2-m1)**2) + gammaln(a2) - gammaln(a1) + (a1*(b2/b1 - 1))
