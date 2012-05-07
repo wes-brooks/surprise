ffb5036502c18c759db3fdc3c875c789b8bbf04a
Wesley Brooks <brooks.wesley@gmail.com>
2012-05-07 08:48:09 -0500

    presentation as given

--- code/misc.py
+++ code/misc.py
@@ -8,5 +8,6 @@ m=0.
 
 cdom = np.loadtxt(fname="../data/2009_CDOM.csv", skiprows=0, dtype=float,unpack=False, delimiter=",")
 pb = np.loadtxt(fname="../data/pheasantbranch-temperature.csv", skiprows=0, dtype=float,unpack=False, delimiter=",")
+gills = np.loadtxt(fname="../data/GillsPondBrookTDS.csv", skiprows=0, dtype=float,unpack=False, delimiter=",")
 
     
